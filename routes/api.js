1daf9d5fe32907ba13b3ea44c1f538131f813446
ImGyo <jywlsh3@naver.com>
2021-08-28 17:14:46 +0900

    프로젝트 구조 변경, nodejs 템플릿 추가

---
+++ routes/api.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+const commonUtil = require('../util/commonUtil');
+const db_info = require('../config/config');
+const dbPool = mysql.createPool(db_info);
+
+//샘플
+router.get("/sample", function(req, res){
+    let resultData = {};
+    let query = `
+        SELECT  *
+        FROM    sample_entity
+    `;
+    dbPool.query(query, (error, results) => {
+        if(error) {
+            commonUtil.sendResponse(res, 500, '1000', error, resultData);
+            return;
+        }
+        let resultData = results[0];
+
+        commonUtil.sendResponse(res, 200, '0', 'sample', resultData);
+    });
+});
+
+
+router.get("/test", (req, res) => {
+    const queryString = parseInt(req.query.sample_id);
+
+
+    let resultData = {};
+    let query = `
+        SELECT  * 
+        FROM    sample_entity
+    `;
+
+    let successCallback = (error, results) => {
+        if(error)
+        {
+            commonUtil.sendResponse(res, 500, '1000', error, resultData);
+            return;
+        }
+
+        let testResult = results.filter(element => element.sample_id == queryString);
+        
+        if(testResult.length === 0){
+            commonUtil.sendResponse(res, 400, '2000', 'Invalid queryString', resultData);
+            return;
+        }
+
+        resultData = testResult[0].sample_name;
+
+        commonUtil.sendResponse(res, 200, '0', 'sample', resultData);
+    };
+
+    dbPool.query(query, successCallback);
+});
+
+// function getConnection() {
+//     return dbPool;
+// };
+
+module.exports = router;
